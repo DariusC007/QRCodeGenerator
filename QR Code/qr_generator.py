407ae1c7d03361f221dd7bee36342ea68a693679
Darius.C <59445082+DariusC007@users.noreply.github.com>
2025-11-12 20:38:59 +0200

    Add files via upload

---
+++ QR Code/qr_generator.py
@@ -0,0 +1,36 @@
+import qrcode
+import tkinter as tk
+from tkinter import messagebox
+from PIL import Image, ImageTk
+
+def generate_qr():
+    url = entry.get()
+    if not url:
+        messagebox.showwarning("Warning", "Enter a valid link!")
+        return
+    img = qrcode.make(url)
+    img.save("qrcode.png")
+
+    # Show image
+    img_tk = ImageTk.PhotoImage(img)
+    label_img.config(image=img_tk)
+    label_img.image = img_tk
+
+    messagebox.showinfo("Succes", "The QR code was generated succesfully!")
+
+# Main window
+root = tk.Tk()
+root.title("QR Code Generator")
+root.geometry("400x400")
+
+# UI
+tk.Label(root, text="Introdu linkul:").pack(pady=10)
+entry = tk.Entry(root, width=40)
+entry.pack(pady=5)
+
+tk.Button(root, text="Generate QR", command=generate_qr).pack(pady=10)
+
+label_img = tk.Label(root)
+label_img.pack(pady=20)
+
+root.mainloop()
